591720d5f7e2557ff3f655783f581fe057a3aac3
SaroarHShahan <saroar.shahan@vivacomsolutions.com>
2020-08-25 11:32:24 +0600

    changed default port and added a welcome message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our home!")
+})
+
 
 app.listen(PORT, () => {
   console.log(`server is connected on port ${PORT}`);
